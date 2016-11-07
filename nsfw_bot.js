bc49dbf1e136ea323fb0bf0a5d3304cc69e34150
K.Panic <Pixelpanic@users.noreply.github.com>
2016-11-07 17:15:45 +0800

    Removed useless comments

--- nsfw_bot.js
+++ nsfw_bot.js
@@ -1,6 +1,5 @@
 var TelegramBot = require('node-telegram-bot-api');
 var token = 'YOUR TOKEN HERE'; //Token here
-//var exec = require('child_process').exec;
 var bot = new TelegramBot(token, {polling: true});
 var fs = require('fs');
 var spawn = require('child_process').spawn;
@@ -10,18 +9,14 @@ bot.onText(/\/nslookup (.+)/, function (msg, match) {
     var chatId = msg.chat.id;
     var messageId = msg.message_id;
     var domain =  match[1];
-    //var output = exec("nslookup " + domain, s);
-    //var data = fs.readFileSync("nslookup.txt");
     var nslookup = spawn("nslookup", [domain]);
 
-
     nslookup.stdout.on('data', (data) => {
-
+        
         console.log(domain);
-
-
         bot.sendMessage(chatId, data, {
         reply_to_message_id: messageId
+            
     });
 
 
@@ -35,18 +30,15 @@ bot.onText(/\/nslookupsoa (.+)/, function (msg, match) {
     var chatId = msg.chat.id;
     var messageId = msg.message_id;
     var domain =  match[1];
-    //var output = exec("nslookup " + domain, s);
-    //var data = fs.readFileSync("nslookup.txt");
     var nslookup = spawn("nslookup", ["-type=soa", domain]);
 
 
     nslookup.stdout.on('data', (data) => {
 
-        console.log(domain);
-
-
+    console.log(domain);
     bot.sendMessage(chatId, data, {
         reply_to_message_id: messageId
+        
     });
 
 
@@ -60,16 +52,12 @@ bot.onText(/\/nslookupmx (.+)/, function (msg, match) {
     var chatId = msg.chat.id;
     var messageId = msg.message_id;
     var domain =  match[1];
-    //var output = exec("nslookup " + domain, s);
-    //var data = fs.readFileSync("nslookup.txt");
     var nslookup = spawn("nslookup", ["-type=mx", domain]);
 
 
     nslookup.stdout.on('data', (data) => {
 
-        console.log(domain);
-
-
+    console.log(domain);
     bot.sendMessage(chatId, data, {
         reply_to_message_id: messageId
     });
@@ -85,16 +73,12 @@ bot.onText(/\/nslookuptxt (.+)/, function (msg, match) {
     var chatId = msg.chat.id;
     var messageId = msg.message_id;
     var domain =  match[1];
-    //var output = exec("nslookup " + domain, s);
-    //var data = fs.readFileSync("nslookup.txt");
     var nslookup = spawn("nslookup", ["-type=txt", domain]);
 
 
     nslookup.stdout.on('data', (data) => {
 
-        console.log(domain);
-
-
+    console.log(domain);
     bot.sendMessage(chatId, data, {
         reply_to_message_id: messageId
     });
@@ -110,16 +94,12 @@ bot.onText(/\/nslookupall (.+)/, function (msg, match) {
     var chatId = msg.chat.id;
     var messageId = msg.message_id;
     var domain =  match[1];
-    //var output = exec("nslookup " + domain, s);
-    //var data = fs.readFileSync("nslookup.txt");
     var nslookup = spawn("nslookup", ["-type=txt","-type=mx","-type=soa", domain]);
 
 
     nslookup.stdout.on('data', (data) => {
 
-        console.log(domain);
-
-
+    console.log(domain);
     bot.sendMessage(chatId, data, {
         reply_to_message_id: messageId
     });
@@ -136,16 +116,14 @@ bot.onText(/\/ping (.+)/, function (msg, match) {
     var chatId = msg.chat.id;
     var messageId = msg.message_id;
     var domain =  match[1];
-    //var output = exec("nslookup " + domain, s);
-    //var data = fs.readFileSync("nslookup.txt");
     var ping = spawn("ping", ["-c","3", domain]);
 
 
     ping.stdout.on('data', (data) => {
-
+        
     bot.sendMessage(chatId, data);
-
     console.log("ping:" + domain);
+        
 });
 
 
